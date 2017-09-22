c805d6b3b4d4f21c4c6e0a3462b7f1c9fa1b59b8
alex-Arc <alex@arc-light.dk>
2017-09-22 22:43:19 +0200

    remove all the Serial stuff to be sure

--- examples/UDPSendReceiveString/UDPSendReceiveString.ino
+++ examples/UDPSendReceiveString/UDPSendReceiveString.ino
@@ -1,6 +1,6 @@
 /*
  UDPSendReceiveString:
- This sketch receives UDP message strings, prints them to the serial port
+ This sketch receives UDP message strings, prints them to the //Serial port
  and sends an "acknowledge" string back to the sender
 
  A Processing sketch is included at the end of file that can be used to send
@@ -39,30 +39,31 @@ void setup() {
   Ethernet.begin(mac, ip);
   Udp.begin(localPort);
 
-  Serial.begin(9600);
+
+  //Serial.begin(9600);
 }
 
 void loop() {
   // if there's data available, read a packet
   int packetSize = Udp.parsePacket();
   if (packetSize) {
-    Serial.print("Received packet of size ");
-    Serial.println(packetSize);
-    Serial.print("From ");
+    ////Serial.print("Received packet of size ");
+    //Serial.println(packetSize);
+    //Serial.print("From ");
     IPAddress remote = Udp.remoteIP();
     for (int i=0; i < 4; i++) {
-      Serial.print(remote[i], DEC);
+      //Serial.print(remote[i], DEC);
       if (i < 3) {
-        Serial.print(".");
+        //Serial.print(".");
       }
     }
-    Serial.print(", port ");
-    Serial.println(Udp.remotePort());
+    //Serial.print(", port ");
+    //Serial.println(Udp.remotePort());
 
     // read the packet into packetBufffer
     Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
-    Serial.println("Contents:");
-    Serial.println(packetBuffer);
+    //Serial.println("Contents:");
+    //Serial.println(packetBuffer);
 
     // send a reply to the IP address and port that sent us the packet we received
     Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
@@ -112,5 +113,3 @@ void loop() {
  println();
  }
  */
-
-
